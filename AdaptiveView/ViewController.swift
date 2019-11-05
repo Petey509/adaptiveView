534b62d03d4e91340c3109a7d9c48e9ffa07ab64
Peterson Cemoin <petersoncemoin@Petersons-MacBook-Pro.local>
2019-11-05 08:18:26 -0500

    initial commit

---
+++ AdaptiveView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ResponsiveApp
+//
+//  Created by Peterson Cemoin on 11/2/19.
+//  Copyright © 2019 YourAreAwesome. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
